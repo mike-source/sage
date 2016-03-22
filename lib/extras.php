b776b3096d1b2f7e45e86536717784bed932ec95
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2016-03-22 15:43:02 +0000

    Remove horrible emoji stuff from head tag in Wordpress 4.2

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,9 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Remove horrible emoji stuff from head tag in Wordpress 4.2
+ */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
