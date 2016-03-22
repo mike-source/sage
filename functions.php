f01bad3f7171960f14d5aa95ccc484d0c675084f
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2016-03-22 15:37:02 +0000

    remove customiser

--- functions.php
+++ functions.php
@@ -15,7 +15,6 @@ $sage_includes = [
   'lib/setup.php',     // Theme setup
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
-  'lib/customizer.php' // Theme customizer
 ];
 
 foreach ($sage_includes as $file) {
