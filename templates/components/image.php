ce17903eec7adf08e6a08ab3d77a441eff03ae0b
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2016-11-28 15:35:14 +0000

    1-1-1 and 1-1-1-1

---
+++ templates/components/image.php
@@ -0,0 +1,2 @@
+
+                <img src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('alt_tag'); ?>" class="content-img">
--- templates/layouts/1-1_col.php
+++ templates/layouts/1-1_col.php
@@ -1,7 +1,7 @@
 
       <div <?php if(get_sub_field('element_id')): ?>id="<?php echo str_replace(' ', '-', strtolower(get_sub_field('element_id'))); ?>" <?php endif;?>class="wrapper col-1-1"<?php if(get_sub_field('bgcolor_enable') || get_sub_field('bgimage_enable')): ?> style="<?php if(get_sub_field('bgcolor_enable')): ?>background-color: <?php the_sub_field('bgcolor'); endif; ?>;<?php if(get_sub_field('bgimage_enable')): ?> background-image: url(<?php the_sub_field('bgimage');?>);<?php endif; ?>"<?php endif; ?>>
 
-          <div class="container col-1__container">
+          <div class="container col-1-1__container">
             <div class="block col-1-1__block-1 content col-1-1__content">
 
 <?php if(have_rows('block_1')): while(have_rows('block_1')): the_row(); get_template_part('templates/blocks/default'); endwhile; endif; ?>
