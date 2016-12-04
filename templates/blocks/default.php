54384a9077112f3a0c8bc9c344aba94213fcb324
Mike Smith <mike@sourcewebdev.co.uk>
2016-12-04 07:30:54 +0000

    text-area html5

--- templates/blocks/default.php
+++ templates/blocks/default.php
@@ -3,10 +3,10 @@
   if(have_rows('components')): while(have_rows('components')): the_row();
       if(get_row_layout() == 'text_area'):
 ?>
-              <div <?php if(get_sub_field('element_id')): ?>id="<?php echo str_replace(' ', '-', strtolower(get_sub_field('element_id'))); ?>" <?php endif;?>class="text-area">
+              <section <?php if(get_sub_field('element_id')): ?>id="<?php echo str_replace(' ', '-', strtolower(get_sub_field('element_id'))); ?>" <?php endif;?>class="text-area">
                 <?php get_template_part('templates/components/text-area'); ?>
 
-              </div>
+              </section>
 <?php endif;
 
       if(get_row_layout() == 'image'):
--- templates/components/text-area.php
+++ templates/components/text-area.php
@@ -1,6 +1,6 @@
 
-                <h1 class="title"><?php the_sub_field('title'); ?></h1>
-                <h2 class="subtitle"><?php the_sub_field('subtitle'); ?></h2>
+                <h2 class="title"><?php the_sub_field('title'); ?></h2>
+                <h3 class="subtitle"><?php the_sub_field('subtitle'); ?></h3>
                 <?php if(get_sub_field('separate_titles')): ?><hr class="title-separator"><?php endif; ?>
 
                 <?php the_sub_field('copy'); ?>
