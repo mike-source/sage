3c145709d5209224e3507140c61c3e73e7d8c99c
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2016-12-05 11:31:08 +0000

    text area if statements

--- templates/components/text-area.php
+++ templates/components/text-area.php
@@ -1,6 +1,6 @@
 
-                <h2 class="title"><?php the_sub_field('title'); ?></h2>
-                <h3 class="subtitle"><?php the_sub_field('subtitle'); ?></h3>
+                <?php if(get_sub_field('title')): ?><h2 class="title"><?php the_sub_field('title'); ?></h2><?php endif; ?>
+                <?php if(get_sub_field('subtitle')): ?><h3 class="subtitle"><?php the_sub_field('subtitle'); ?></h3><?php endif; ?>
                 <?php if(get_sub_field('separate_titles')): ?><hr class="title-separator"><?php endif; ?>
 
-                <?php the_sub_field('copy'); ?>
+                <?php if(get_sub_field('copy')): ?><?php the_sub_field('copy'); ?><?php endif; ?>
