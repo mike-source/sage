c0e83b4ba014dec9c5cdacb2ce1ff788d10fb32e
Mike Smith (Office) <mike@sourcewebdev.co.uk>
2017-02-24 12:00:00 +0000

    post title / entry meta

--- templates/components/sidebar.php
+++ templates/components/sidebar.php
@@ -1,3 +1,8 @@
+<header>
+  <h1 class="h2 entry-title"><?php the_title(); ?></h1>
+  <?php get_template_part('templates/components/entry-meta'); ?>
+</header>
+
 <?php get_template_part('templates/components/news--small'); ?>
 
 <?php dynamic_sidebar('sidebar-primary'); ?>
