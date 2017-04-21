e1606bfcf2906efa4c6df79907b4a121a62431e0
Mike Smith <mike@sourcewebdev.co.uk>
2017-04-21 15:19:01 +0100

    video-embed php template

---
+++ templates/components/video-embed.php
@@ -0,0 +1,5 @@
+
+                <div class="videoWrapper aspect-<?php the_sub_field('aspect_ratio'); ?>">
+                  <!-- Copy & Pasted from YouTube -->
+                  <iframe src="http://www.youtube.com/embed/<?php the_sub_field('embed_code'); ?>?rel=0&hd=1" frameborder="0" allowfullscreen></iframe>
+                </div>
